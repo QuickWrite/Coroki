32ad5a04ccd8789177ececc0640d3d78c61cfdb6
QuickWrite <git@quickwrite.net>
2026-07-14 23:02:09 +0200

    Use Gin framework for routing
    As the Gin framework allows for more possibilities like middleware, the router was switched.

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,22 +6,26 @@ import (
 	"github.com/QuickWrite/Coroki/internal/data"
 	db "github.com/QuickWrite/Coroki/internal/db/sqlc/gen"
 	"github.com/QuickWrite/Coroki/internal/web/user"
+	"github.com/gin-gonic/gin"
 )
 
 func Routes(context data.ServerContext) http.Handler {
-	mux := http.NewServeMux()
+	r := gin.Default()
 
 	query := db.New(context.DB)
 
 	userRouter := user.New(user.NewDbUserStore(query))
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("This is fine"))
+	r.GET("/healthz", func(c *gin.Context) {
+		c.Writer.WriteHeader(http.StatusOK)
+		c.Writer.Write([]byte("This is fine"))
 	})
 
-	// Two temporary routes to test out the database
-	mux.HandleFunc("GET /users", userRouter.HandleGetUsers(context))
+	// Temporary route to test out the database
+	getUsers := userRouter.HandleGetUsers(context)
+	r.GET("/users", func(ctx *gin.Context) {
+		getUsers(ctx.Writer, ctx.Request)
+	})
 
-	return mux
+	return r
 }
