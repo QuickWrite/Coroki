3e7475835afd64da2887edbf86e7d8f244f77b70
QuickWrite <git@quickwrite.net>
2026-09-02 17:50:15 +0200

    Fix: Corrected parameter names in Interface method
    It seems like the parameter names in the interface method Compare() was
    forgotten.
    As they both are strings the code still compiled, but semantically there
    was still the issue. As such they are now swapped.

--- internal/data/security.go
+++ internal/data/security.go
@@ -3,6 +3,6 @@ package data
 type PasswordHasher interface {
 	Name() string
 	Hash(password string) (string, error)
-	Compare(encodedHash string, password string) error
+	Compare(password string, encodedHash string) error
 	NeedsRehash(encodedHash string) bool
 }
